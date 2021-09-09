7654086cd49fab2aa311ce2b5dea87dbced15e09
Fanil <Fanil.Biktimirov@x5.ru>
2021-09-09 18:46:18 +0300

    Enable Eureka Server

--- src/main/java/ru/education_platform_jma/jmaeurekaserver/JmaEurekaServerApplication.java
+++ src/main/java/ru/education_platform_jma/jmaeurekaserver/JmaEurekaServerApplication.java
@@ -2,7 +2,9 @@ package ru.education_platform_jma.jmaeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class JmaEurekaServerApplication {
 
