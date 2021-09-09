01039cb1aac13ab064fbd29d9bf3ffa712b17664
Sergey Melnikov <74730932+pdasilem@users.noreply.github.com>
2021-09-09 13:41:57 +0500

    Add files via upload

---
+++ src/main/java/ru/education_platform_jma/jmaeurekaserver/JmaEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.education_platform_jma.jmaeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmaEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmaEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/education_platform_jma/jmaeurekaserver/JmaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.education_platform_jma.jmaeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmaEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
